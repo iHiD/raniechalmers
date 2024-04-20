8c009d6a06ba095eebd734f039bbc7111e28349b
Jeremy Walker <jez.walker@gmail.com>
2024-04-20 22:15:50 +0100

    Fix more

--- src/FuneralPage.jsx
+++ src/FuneralPage.jsx
@@ -2,6 +2,7 @@ function FuneralPage() {
   return (
     <>
       <h1>Funeral Details</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d294325.67313906754!2d-2.244549610937493!3d54.80774790000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487e7f6d8965e057%3A0x7b6f960cdd00e5be!2sCounty%20Durham%20Funeral%20Directors%20Ltd!5e0!3m2!1sen!2suk!4v1713646759796!5m2!1sen!2suk" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </>
   );
 }
